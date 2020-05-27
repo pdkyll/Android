95a8cab105b90f789f39331f3992a50e4711e03f
thkim0118 <choir0118@gmail.com>
2020-05-27 23:21:24 +0900

    영화 목록 페이저뷰 추가

---
+++ app/src/main/java/com/taehyungkim/project_a/ui/MovieListFragment.java
@@ -0,0 +1,94 @@
+package com.taehyungkim.project_a.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.taehyungkim.project_a.R;
+import com.taehyungkim.project_a.ui.adapter.MovieListAdapter;
+
+import java.util.ArrayList;
+
+public class MovieListFragment extends Fragment {
+
+    private ArrayList<Integer> movie_view = new ArrayList<>();
+    private ArrayList<String> movie_name = new ArrayList<>();
+    private ArrayList<String> book_rate = new ArrayList<>();
+    private ArrayList<String> age = new ArrayList<>();
+    private ArrayList<String> date = new ArrayList<>();
+
+    ViewPager2 viewPager2;
+    MovieListAdapter movieListAdapter;
+
+    Context context;
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_movie_list, container, false);
+
+        viewPager2 = (ViewPager2) rootView.findViewById(R.id.viewPager);
+
+        movie_view.add(R.drawable.image1);
+        movie_name.add("1. 군 도");
+        book_rate.add("61.6%");
+        age.add("15세 관람가");
+        date.add("D-1");
+
+        movie_view.add(R.drawable.image2);
+        movie_name.add("2. 공 조");
+        book_rate.add("61.6%");
+        age.add("15세 관람가");
+        date.add("D-1");
+
+        movie_view.add(R.drawable.image3);
+        movie_name.add("3. 더 킹");
+        book_rate.add("61.6%");
+        age.add("15세 관람가");
+        date.add("D-1");
+
+        movie_view.add(R.drawable.image4);
+        movie_name.add("4. 레지던트 이블");
+        book_rate.add("61.6%");
+        age.add("15세 관람가");
+        date.add("D-1");
+
+        movie_view.add(R.drawable.image5);
+        movie_name.add("5. 럭키");
+        book_rate.add("61.6%");
+        age.add("15세 관람가");
+        date.add("D-1");
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        movieListAdapter = new MovieListAdapter(movie_view, movie_name, book_rate, age, date);
+        viewPager2.setAdapter(movieListAdapter);
+        viewPager2.setOffscreenPageLimit(3);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/com/taehyungkim/project_a/ui/adapter/MovieListAdapter.java
@@ -0,0 +1,73 @@
+package com.taehyungkim.project_a.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.taehyungkim.project_a.R;
+
+import java.util.ArrayList;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieViewHolder> {
+
+    private ArrayList<Integer> movie_view;
+    private ArrayList<String> movie_name;
+    private ArrayList<String> book_rate;
+    private ArrayList<String> age;
+    private ArrayList<String> date;
+
+    public MovieListAdapter(ArrayList<Integer> movie_view, ArrayList<String> movie_name,
+                            ArrayList<String> book_rate, ArrayList<String> age, ArrayList<String> date) {
+        this.movie_view = movie_view;
+        this.movie_name = movie_name;
+        this.book_rate = book_rate;
+        this.age = age;
+        this.date = date;
+    }
+
+    @NonNull
+    @Override
+    public MovieListAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie_list, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieListAdapter.MovieViewHolder holder, int position) {
+        holder.movie_view.setImageResource(movie_view.get(position));
+        holder.movie_name.setText(movie_name.get(position));
+        holder.book_rate.setText(book_rate.get(position));
+        holder.age.setText(age.get(position));
+        holder.date.setText(date.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movie_view.size();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        private ImageView movie_view;
+        private TextView movie_name;
+        private TextView book_rate;
+        private TextView age;
+        private TextView date;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            movie_view = (ImageView) itemView.findViewById(R.id.movie_view);
+            movie_name = (TextView) itemView.findViewById(R.id.movie_name_text);
+            book_rate = (TextView) itemView.findViewById(R.id.book_rate_text);
+            age = (TextView) itemView.findViewById(R.id.age_text);
+            date = (TextView) itemView.findViewById(R.id.date_text);
+        }
+    }
+}
