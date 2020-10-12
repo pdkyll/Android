44549d27b807e7842a67c918c33e313fb3845391
junior dev <56363632+thkim0118@users.noreply.github.com>
2020-10-13 00:41:38 +0900

    ADD checking network state & EDIT Observable to Single Jira - TM-3, TM-7
     - 로그인 시 network 가 연결이 되어있을 때만 작동하도록 check network 기능 구현.

--- app/src/main/java/com/thkim/market/api/SignInApi.kt
+++ app/src/main/java/com/thkim/market/api/SignInApi.kt
@@ -1,20 +1,51 @@
 package com.thkim.market.api
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.GoogleAuthProvider
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 /*
  * Created by Thkim on 2020/10/11
  */
 class SignInApi {
 
-    fun getAuthCredential(idToken: String): Observable<AuthCredential> = Observable.create {
+    fun getAuthCredential(idToken: String): Single<AuthCredential> = Single.create {
         if (!it.isDisposed) {
-            it.onNext(GoogleAuthProvider.getCredential(idToken, null))
-            it.onComplete()
+            it.onSuccess(GoogleAuthProvider.getCredential(idToken, null))
         } else {
             it.onError(Throwable("SignInApi has error."))
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    fun getNetworkState(context: Context): Single<Boolean> = Single.create {
+        if (!it.isDisposed) {
+            try {
+                val connectivityManager =
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//                val builder = NetworkRequest.Builder()
+                connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        Log.d("Thkim_", "onAvailable() -> internet")
+                        it.onSuccess(true)
+                    }
+
+                    override fun onLost(network: Network) {
+                        Log.d("Thkim_", "onLost() -> internet")
+                        it.onSuccess(false)
+                    }
+                }
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                it.onError(Throwable("getNetworkState() has error."))
+            }
         }
     }
 }
--- app/src/main/java/com/thkim/market/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/thkim/market/ui/signin/SignInViewModel.kt
@@ -1,6 +1,9 @@
 package com.thkim.market.ui.signin
 
+import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -8,6 +11,7 @@ import com.thkim.market.api.SignInApi
 import com.thkim.market.util.SupportOptional
 import com.thkim.market.util.optionalOf
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Consumer
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import io.reactivex.rxjava3.subjects.PublishSubject
@@ -32,6 +36,8 @@ class SignInViewModel(
     // 작업 진행 상태를 전달할 서브젝트입니다. 초깃값으로 false 를 지정합니다.
     val isLoading: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
 
+    val isConnected: PublishSubject<Boolean> = PublishSubject.create()
+
     fun requestSignInAccount(idToken: String, auth: FirebaseAuth): Disposable =
         signInApi.getAuthCredential(idToken)
             .subscribeOn(Schedulers.io())
@@ -49,4 +55,11 @@ class SignInViewModel(
                 message.onNext(it.message ?: "Unexpected error.")
             }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun getNetworkState(context: Context): Disposable =
+        signInApi.getNetworkState(context)
+            .subscribeOn(Schedulers.computation())
+            .subscribe(Consumer {
+                isConnected.onNext(it)
+            })
 }
