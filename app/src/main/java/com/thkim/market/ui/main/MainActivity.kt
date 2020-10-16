ef7d03505cb4d71f3e60f9381982fc6284aed3dd
TaehyungKim_DeskTop <choir0118@gmail.com>
2020-10-16 23:58:41 +0900

    Init master branch

--- app/src/main/java/com/thkim/market/ui/main/MainActivity.kt
+++ app/src/main/java/com/thkim/market/ui/main/MainActivity.kt
@@ -3,11 +3,14 @@ package com.thkim.market.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.thkim.market.R
+import com.thkim.util.DLog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        DLog.end()
     }
 }
